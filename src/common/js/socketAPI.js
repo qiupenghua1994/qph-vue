f032fe05e11b1fd626ecd427a44f0c6b32978958
ph.qiu <947946592@163.com>
2018-10-13 15:18:20 +0800

    完善聊天功能以及一个登陆页面

--- src/common/js/socketAPI.js
+++ src/common/js/socketAPI.js
@@ -1,5 +1,6 @@
 import io from 'socket.io-client'
 
-var socket = io(location.hostname + ':8080');
+// var socket = io(location.hostname + ':8080');
+var socket = io('39.104.124.138' + ':8080');
 
 export default socket
